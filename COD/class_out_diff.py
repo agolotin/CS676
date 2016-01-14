13286e11cf1995a0368395ed41d82e9619034362
Artem Golotin <artem.golotin@gmail.com>
2016-01-13 21:23:32 -0700

    Final commit

--- COD/class_out_diff.py
+++ COD/class_out_diff.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
-#import shutil #import tempfile
+#import shutil 
+#import tempfile
 import csv
 
 from sklearn import datasets
@@ -15,39 +16,35 @@ from os.path import join
 from random import randint
 import numpy as np
 
-def crossValidationSplit(seed, dataset):
+def crossValidationSplit(seed, dataset, target_set, split=0.2):
     train_set, test_set, train_target, test_target = ([], [], [], [])
 
-    rs = cross_validation.ShuffleSplit(dataset.data.shape[0], n_iter=1, test_size=0.2, random_state=seed)
+    rs = cross_validation.ShuffleSplit(len(dataset), n_iter=1, test_size=split, random_state=seed)
+
 
     for train_index, test_index in rs:
         for train_i, test_i in zip_long(train_index, test_index):
             # Get training set instances
-            train_set.append(dataset.data[train_i])
-            train_target.append(dataset.target[train_i])
+            train_set.append(dataset[train_i])
+            train_target.append(target_set[train_i])
             # Get test set instances
             if test_i != None:
-               test_set.append(dataset.data[test_i])
-               test_target.append(dataset.target[test_i])
+               test_set.append(dataset[test_i])
+               test_target.append(target_set[test_i])
 
-    return train_set, test_set, train_target, test_target
+    if split == 0.2:
+        return train_set, test_set, train_target, test_target
+    return train_set, train_target
 
-def runClassifiers(train_set, train_target, test_data, test_target):
-    # Naive Bayes
-    gauss_nb = GaussianNB()
-    nb_model = gauss_nb.fit(train_set, train_target)
+def runClassifiers(train_set, train_target, test_data):
+    # Decision Tree
+    #gauss_nb = GaussianNB()
+    #nb_pred = gauss_nb.fit(train_set, train_target).predict(test_data)
 
-    nb_pred = nb_model.predict(test_data)
-    nb_score = nb_model.score(test_data, test_target)
-
-    # Decision tree
     dec_tree = DecisionTreeClassifier()
-    dt_model = dec_tree.fit(train_set, train_target)
+    dt_pred = dec_tree.fit(train_set, train_target).predict(test_data)
 
-    dt_pred = dt_model.predict(test_data)
-    dt_score = dt_model.score(test_set, test_target)
-
-    return nb_pred, dt_pred, nb_score, dt_score
+    return dt_pred
 
 
 ''' Load a general CSV file and return a Bunch data structure
@@ -65,32 +62,30 @@ def loadCSV(dest_dir, file_name, i=0):
     data = raw_data[:, :-1]
     target = raw_data[:, -1]
 
-#    data = np.empty((n_samples, n_features))
-#    target = np.empty((n_samples,))
-#
-#    for sample in raw_data:
-#        data[i] = np.asarray(sample[:-1], dtype="float")
-#        target[i] = np.asarray(sample[-1], dtype="float")
-#        i += 1
-
     return Bunch(data=data, target=target)
 
 
+def computeCOD(nb_pred1, nb_pred2, N):
+    cod = 0
+    for pred1, pred2 in zip(nb_pred1, nb_pred2):
+        cod += 1 if pred1 != pred2 else 0
 
-def computeCOD(nb_pred, dt_pred, targets, N):
-    # number of instances where h1 is correct, but h2 is incorrect
-    n10 = 0
-    # number of instances where h2 is correct, but h1 is incorrect
-    n01 = 0
-    # number of instances where both h1 and h2 are uncorrect, and they make different predictions
-    n00D = 0
+    return float(cod) / float(N)
 
-    for pred1, pred2, t in zip(nb_pred, dt_pred, targets):
-        n10 += 1 if pred1 == t and pred2 != t else 0
-        n01 += 1 if pred1 != t and pred2 == t else 0
-        n00D += 1 if pred1 != t and pred2 != t and pred1 != pred2 else 0
-
-    return float(n10 + n01 + n00D) / float(N)
+#def computeCOD(nb_pred, dt_pred, targets, N):
+#    # number of instances where h1 is correct, but h2 is incorrect
+#    n10 = 0
+#    # number of instances where h2 is correct, but h1 is incorrect
+#    n01 = 0
+#    # number of instances where both h1 and h2 are uncorrect, and they make different predictions
+#    n00D = 0
+#
+#    for pred1, pred2, t in zip(nb_pred, dt_pred, targets):
+#        n10 += 1 if pred1 == t and pred2 != t else 0
+#        n01 += 1 if pred1 != t and pred2 == t else 0
+#        n00D += 1 if pred1 != t and pred2 != t and pred1 != pred2 else 0
+#
+#    return float(n10 + n01 + n00D) / float(N)
 
 
 
@@ -100,14 +95,8 @@ if __name__ == "__main__":
     # methods = [method for method in dir(datasets) if callable(getattr(datasets, method)) and "load" in method]
     #custom_data_home = tempfile.mkdtemp()
     dataset_dir_home = '/home/salt/Desktop/CS676/COD/datasets'
-
-    '''
-    diabetes: (768 instances, 8 attributes)
-    Daily and Sports activity: (9120 instances, 5625 attributes)
-    '''
-
     
-    dataset_names = ['sensorless_drive_diagnosis', 'diabetes', 'pima-indians-diabetes', 'iris']#['MNIST Original', 'leukemia', 'diabetes']
+    dataset_names = ['sensorless_drive_diagnosis', 'diabetes', 'pima-indians-diabetes', 'iris']
 
     cod_vector = list()
     for name in dataset_names:
@@ -117,25 +106,37 @@ if __name__ == "__main__":
         print "Dataset {0} is loaded, shape {1}".format(name.upper().split('/')[-1], dataset.data.shape)
 
         dataset_cod_vector = list()
+        # h1
+        seed = randint(0, dataset.data.shape[0])
+        train_set, test_set, train_target, test_target = crossValidationSplit(seed, dataset.data, dataset.target)
+
+        nb_pred1 = runClassifiers(train_set, train_target, test_set)
+        model_list = [nb_pred1]
+
         for i in xrange(10):
-
+            # h2
             seed = randint(0, dataset.data.shape[0])
-            train_set, test_set, train_target, test_target = crossValidationSplit(seed, dataset)
+            train_set, train_target = crossValidationSplit(seed, train_set, train_target, 0)
 
-            nb_pred, dt_pred, nb_score, dt_score = runClassifiers(train_set, train_target, test_set, test_target)
+            nb_pred2 = runClassifiers(train_set, train_target, test_set)
+            
+            # Compute pairwise COD
+            t_cod_vector = list()
+            for pred_model in model_list:
+                COD = computeCOD(pred_model, nb_pred2, len(test_target))
+                t_cod_vector.append(COD)
+                cod_vector.append(COD)
+                dataset_cod_vector.append(COD)
 
-            COD = computeCOD(nb_pred, dt_pred, test_target, len(test_target))
-            cod_vector.append(COD)
-            dataset_cod_vector.append(COD)
+            model_list.append(nb_pred2)
 
-            print ("%d, %f, %f, %f"
-                    % (seed, nb_score, dt_score, COD))
+            print "{0}".format(t_cod_vector)
 
-        print ("Average COD between Naive Bayes and Decision Tree for dataset %s: %f"
+        print ("Average COD for Decision Tree for dataset %s: %f"
                 % (name.upper().split('/')[-1], sum(dataset_cod_vector) / float(len(dataset_cod_vector))))
         print # Separate datasets by new line
     
-    print ("Average COD between Naive Bayes and Decision Tree: %f"
+    print ("Average COD between Decision Tree: %f"
             % (sum(cod_vector) / float(len(cod_vector))))
 
     # Clean up the temporary directory
